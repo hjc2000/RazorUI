17e3f688a56f2c709e9e8f9352c1d25bd7f1c28f
hjc2000 <hjc.666@foxmail.com>
2024-07-19 11:59:27 +0800

    export function DisableZoom()

--- RazorUI/DisableZoom.razor.js
+++ RazorUI/DisableZoom.razor.js
@@ -1,36 +1,42 @@
-﻿// 这里的代码用来禁用 鼠标滚轮缩放、双指滑动缩放
+﻿/**
+ * 禁用 鼠标滚轮缩放、双指滑动缩放
+ */
 export function DisableZoom()
 {
-	window.addEventListener('mousewheel', function (event)
-	{
-		if (event.ctrlKey === true || event.metaKey)
+	window.addEventListener('mousewheel',
+		function (event)
 		{
-			event.preventDefault();
-		}
-	},
+			if (event.ctrlKey === true || event.metaKey)
+			{
+				event.preventDefault();
+			}
+		},
 		{ passive: false });
 
-	window.addEventListener('touchstart', function (event)
-	{
-		if (event.touches.length >= 2)
+	window.addEventListener('touchstart',
+		function (event)
 		{
-			event.preventDefault();
-		}
-	});
+			if (event.touches.length >= 2)
+			{
+				event.preventDefault();
+			}
+		});
 
-	window.addEventListener('touchmove', function (event)
-	{
-		if (event.touches.length >= 2)
+	window.addEventListener('touchmove',
+		function (event)
 		{
-			event.preventDefault();
-		}
-	});
+			if (event.touches.length >= 2)
+			{
+				event.preventDefault();
+			}
+		});
 
-	window.addEventListener('touchend', function (event)
-	{
-		if (event.touches.length >= 2)
+	window.addEventListener('touchend',
+		function (event)
 		{
-			event.preventDefault();
-		}
-	});
+			if (event.touches.length >= 2)
+			{
+				event.preventDefault();
+			}
+		});
 }
