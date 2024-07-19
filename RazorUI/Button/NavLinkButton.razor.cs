92b5c656b3a6521293ef45b0b5a506d2316cc11d
hjc2000 <hjc.666@foxmail.com>
2024-07-19 14:31:01 +0800

    <Version>1.2.0</Version>

---
+++ RazorUI/Button/NavLinkButton.razor.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace RazorUI.Button;
+public partial class NavLinkButton
+{
+	[Parameter]
+	public string Url { get; set; } = "./";
+
+	[Parameter]
+	public RenderFragment? Left { get; set; }
+	[Parameter]
+	public RenderFragment? Right { get; set; }
+}
---
+++ RazorUI/Button/STButton.razor.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace RazorUI.Button;
+public partial class STButton
+{
+	[Parameter]
+	public RenderFragment? ChildContent { get; set; }
+	[Parameter]
+	public bool Disabled { get; set; } = false;
+
+	[Parameter(CaptureUnmatchedValues = true)]
+	public Dictionary<string, object>? CaptureUnmatchedValues { get; set; }
+}
