c0608209986e76a52f574c320934665e8e82a275
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2024-07-19 18:52:01 +0800

    module = await JS.InvokeAsync<IJSObjectReference>("import",                 "./_content/RazorUI/WebConsole.razor.js");

--- RazorUI/WebConsole.razor.cs
+++ RazorUI/WebConsole.razor.cs
@@ -5,8 +5,8 @@ public partial class WebConsole
 {
 	protected override async Task OnInitializedAsync()
 	{
-		module = await JS.InvokeAsync<IJSObjectReference>("import",
-			"./_content/RazorUI/WebConsole.razor.js");
+		module = await JS.InvokeAsync<IJSObjectReference>("import", 
+														  "./_content/RazorUI/WebConsole.razor.js");
 	}
 
 	public async Task LogAsync(string message)
