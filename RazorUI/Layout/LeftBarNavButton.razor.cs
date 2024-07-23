15f29791940b81e097f5125c5fd8a8376fb0840d
hjc2000 <hjc.666@foxmail.com>
2024-07-23 13:13:30 +0800

    public partial class LeftBarNavButton

--- RazorUI/Layout/LeftBarNavButton.razor.cs
+++ RazorUI/Layout/LeftBarNavButton.razor.cs
@@ -42,26 +42,30 @@ public partial class LeftBarNavButton
 
 	/// <summary>
 	///		只要 CurrentRelativeUri 是 RelativeHrefUri 的子路径就应该聚焦。
+	///		因为左边按钮条是用来做一级导航的，左边菜单做二级导航。
 	/// </summary>
 	private bool ShouldFocus
 	{
 		get
 		{
-			if (!CurrentRelativeUri.StartsWith(RelativeHrefUri))
+			string current_relative_uri = CurrentRelativeUri;
+			string relative_href_uri = RelativeHrefUri;
+
+			if (!current_relative_uri.StartsWith(relative_href_uri))
 			{
 				return false;
 			}
 
-			if (RelativeHrefUri == CurrentRelativeUri)
+			if (relative_href_uri == current_relative_uri)
 			{
 				return true;
 			}
 
-			// 现在是 CurrentRelativeUri 比 RelativeHrefUri 长
-			if (CurrentRelativeUri[RelativeHrefUri.Length] != '/')
+			// 现在是 current_relative_uri 比 relative_href_uri 长
+			if (current_relative_uri[relative_href_uri.Length] != '/')
 			{
-				/* 只有 CurrentRelativeUri 比 RelativeHrefUri 多出的部分是以路径分隔符开头，
-				 * 才能说明 CurrentRelativeUri 是 RelativeHrefUri 的子路径
+				/* 只有 current_relative_uri 比 relative_href_uri 多出的部分是以路径分隔符开头，
+				 * 才能说明 current_relative_uri 是 relative_href_uri 的子路径
 				 */
 				return false;
 			}
