5cef7db87b400b3f500841d0fcb14459162b0030
hjc2000 <hjc.666@foxmail.com>
2024-07-24 13:19:17 +0800

    public partial class BoolSelectElement

---
+++ RazorUI/输入框/BoolSelectElement.razor.cs
@@ -0,0 +1,38 @@
+using Microsoft.AspNetCore.Components;
+
+namespace RazorUI.输入框;
+
+/// <summary>
+///		布尔选择框。
+/// </summary>
+public partial class BoolSelectElement
+{
+	/// <summary>
+	///		绑定值。
+	/// </summary>
+	[Parameter]
+	public bool Value { get; set; }
+
+	/// <summary>
+	///		绑定值。
+	/// </summary>
+	[Parameter]
+	public EventCallback<bool> ValueChanged { get; set; }
+
+	private async Task OnChange(bool value)
+	{
+		await ValueChanged.InvokeAsync(value);
+	}
+
+	/// <summary>
+	///		true 显示在下拉列表中的值。
+	/// </summary>
+	[Parameter]
+	public string TrueText { get; set; } = "true";
+
+	/// <summary>
+	///		false 显示在下拉列表中的值。
+	/// </summary>
+	[Parameter]
+	public string FalseText { get; set; } = "false";
+}
