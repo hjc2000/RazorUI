9a6df29740dca04c4e589da5e0681754b86d9c14
hjc2000 <hjc.666@foxmail.com>
2024-07-25 09:01:28 +0800

    <Version>2.9.3</Version>

--- RazorUI/Layout/LeftBarNavButton.razor.cs
+++ RazorUI/Layout/LeftBarNavButton.razor.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using JCNET.字符串处理;
+using Microsoft.AspNetCore.Components;
 
 namespace RazorUI.Layout;
 
@@ -41,28 +42,6 @@ public partial class LeftBarNavButton
 	}
 
 	/// <summary>
-	///		Href 转化为的相对于 base 标签中指定的基路径的相对路径。
-	/// </summary>
-	private string RelativeHrefUri
-	{
-		get
-		{
-			return Nav.ToBaseRelativePath(AbsoluteHrefUri);
-		}
-	}
-
-	/// <summary>
-	///		导航系统当前所处的绝对 URI 相对于基础 URI 的相对 URI。
-	/// </summary>
-	private string CurrentRelativeUri
-	{
-		get
-		{
-			return Nav.ToBaseRelativePath(Nav.Uri);
-		}
-	}
-
-	/// <summary>
 	///		只要 CurrentRelativeUri 是 RelativeHrefUri 的子路径就应该聚焦。
 	///		因为左边按钮条是用来做一级导航的，左边菜单做二级导航。
 	/// </summary>
@@ -70,29 +49,8 @@ public partial class LeftBarNavButton
 	{
 		get
 		{
-			string current_relative_uri = CurrentRelativeUri;
-			string relative_href_uri = RelativeHrefUri;
-
-			if (!current_relative_uri.StartsWith(relative_href_uri))
-			{
-				return false;
-			}
-
-			if (relative_href_uri == current_relative_uri)
-			{
-				return true;
-			}
-
-			// 现在是 current_relative_uri 比 relative_href_uri 长
-			if (current_relative_uri[relative_href_uri.Length] != '/')
-			{
-				/* 只有 current_relative_uri 比 relative_href_uri 多出的部分是以路径分隔符开头，
-				 * 才能说明 current_relative_uri 是 relative_href_uri 的子路径
-				 */
-				return false;
-			}
-
-			return true;
+			return new RoutePath(new Uri(Nav.Uri)) <=
+				new RoutePath(new Uri(AbsoluteHrefUri));
 		}
 	}
 }
