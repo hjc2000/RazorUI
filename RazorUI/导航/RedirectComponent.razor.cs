eb0d1b0484689f69e07cb11781dc1000de462473
hjc2000 <hjc.666@foxmail.com>
2024-07-26 08:43:45 +0800

    public partial class RedirectComponent 改成使用  protected override async Task OnParametersSetAsync() 进行重定向。因为如果是同一页面之间的导航，例如两个URL被同一个带有路由参数的页面捕获，将不会触发OnInitialize事件

--- RazorUI/导航/RedirectComponent.razor.cs
+++ RazorUI/导航/RedirectComponent.razor.cs
@@ -6,13 +6,10 @@ namespace RazorUI.导航;
 /// </summary>
 public partial class RedirectComponent
 {
-	/// <summary>
-	///		初始化。会进行重定向。
-	/// </summary>
-	/// <returns></returns>
-	protected override async Task OnInitializedAsync()
+	/// <inheritdoc/>
+	protected override async Task OnParametersSetAsync()
 	{
-		await base.OnInitializedAsync();
+		await base.OnParametersSetAsync();
 		string current_relative_path = Nav.ToBaseRelativePath(Nav.Uri);
 		string dst_path = RedirectUriProvider.GetRedirectUri(current_relative_path);
 		Nav.NavigateTo(dst_path);
---
+++ Test/Pages/CatchAllPage.razor.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Threading.Tasks;
+
+namespace Test.Pages;
+
+public partial class CatchAllPage
+{
+	protected override async Task OnParametersSetAsync()
+	{
+		await base.OnParametersSetAsync();
+		Console.WriteLine($"CatchAllPage 将 {Nav.Uri} 捕获到 {nameof(Path)}，{nameof(Path)} = {Path}");
+	}
+
+	[Parameter]
+	public string Path { get; set; } = string.Empty;
+
+	[Inject]
+	private NavigationManager Nav { get; set; } = default!;
+}
