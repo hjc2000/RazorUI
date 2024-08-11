9acab7ce29d6848aa9c57e1ae542ef5f5a179625
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2024-08-12 00:10:01 +0800

    BarChart

--- RazorUI/Echarts/BarChart.razor.cs
+++ RazorUI/Echarts/BarChart.razor.cs
@@ -1,5 +1,71 @@
-﻿namespace RazorUI.Echarts;
+﻿
+using JSInteropLib;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
 
-public partial class BarChart
+namespace RazorUI.Echarts;
+
+public partial class BarChart :
+	IJSObjectProjection,
+	IAsyncDisposable
 {
+	private bool _disposed = false;
+
+	/// <inheritdoc/>
+	public async ValueTask DisposeAsync()
+	{
+		if (_disposed)
+		{
+			return;
+		}
+
+		_disposed = true;
+		GC.SuppressFinalize(this);
+
+		if (Projection is not null)
+		{
+			await Projection.InvokeVoidAsync("Dispose");
+			Console.WriteLine("释放 js 的 BarChart 对象");
+			await Projection.DisposeAsync();
+		}
+
+		if (_jsop is not null)
+		{
+			await _jsop.DisposeAsync();
+		}
+
+		if (_jsm is not null)
+		{
+			await _jsm.DisposeAsync();
+		}
+	}
+
+	/// <inheritdoc/>
+	protected override async Task OnAfterRenderAsync(bool firstRender)
+	{
+		await base.OnAfterRenderAsync(firstRender);
+		if (!firstRender)
+		{
+			return;
+		}
+
+		_jsop = new JSOp(JS);
+		await _jsop.AddScriptAsync("./_content/RazorUI/echarts.min.js");
+		_jsm = new JSModule(JS, "./_content/RazorUI/Echarts/BarChart.razor.js");
+		Projection = await _jsm.InvokeAsync<IJSObjectReference>("BarChart.Create", _chart_container);
+		await Projection.InvokeVoidAsync("Draw");
+	}
+
+	[Inject]
+	private IJSRuntime JS { get; set; } = default!;
+
+	private ElementReference _chart_container;
+	private JSOp _jsop = default!;
+	private JSModule _jsm = default!;
+
+	/// <summary>
+	///		js 侧的镜像
+	/// </summary>
+	public IJSObjectReference Projection { get; private set; } = default!;
+
 }
--- Test/Pages/CatchAllPage.razor.cs
+++ Test/Pages/CatchAllPage.razor.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.AspNetCore.Components;
 using RazorUI.导航;
-using System;
 using System.Threading.Tasks;
 
 namespace Test.Pages;
@@ -10,15 +9,11 @@ public partial class CatchAllPage
 	protected override async Task OnParametersSetAsync()
 	{
 		await base.OnParametersSetAsync();
-		Console.WriteLine($"CatchAllPage 将 {Nav.Uri} 捕获到 {nameof(Path)}，{nameof(Path)} = {Path}");
 		Redirector.Redirect();
 	}
 
 	[Parameter]
 	public string Path { get; set; } = string.Empty;
-
-	[Inject]
-	private NavigationManager Nav { get; set; } = default!;
 
 	[Inject]
 	private Redirector Redirector { get; set; } = default!;
