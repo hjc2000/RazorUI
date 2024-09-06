ded0e50f959177dc8a2b70367acc14af011ef6ba
hjc2000 <hjc.666@foxmail.com>
2024-09-06 10:16:56 +0800

    FormExpandButton

---
+++ RazorUI/表单/FormExpandButton.razor.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace RazorUI.表单;
+
+/// <summary>
+///		表单展开按钮
+/// </summary>
+public partial class FormExpandButton
+{
+	/// <summary>
+	///		展开状态
+	/// </summary>
+	[Parameter]
+	public FormExpandState ExpandState { get; set; }
+
+	/// <summary>
+	///		展开状态发生变化时的回调
+	/// </summary>
+	[Parameter]
+	public EventCallback<FormExpandState> ExpandStateChanged { get; set; }
+
+	private async Task ChangeExpandStateAsync()
+	{
+		if (ExpandState == FormExpandState.Expand)
+		{
+			await ExpandStateChanged.InvokeAsync(FormExpandState.Collapse);
+		}
+		else
+		{
+			await ExpandStateChanged.InvokeAsync(FormExpandState.Expand);
+		}
+	}
+}
+
+/// <summary>
+///		表单展开状态
+/// </summary>
+public enum FormExpandState
+{
+	/// <summary>
+	///		折叠
+	/// </summary>
+	Collapse,
+
+	/// <summary>
+	///		展开
+	/// </summary>
+	Expand,
+}
